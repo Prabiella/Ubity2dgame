d21d77c03a93b9b6a14add9d756f6059e534236d
admin <paccote@hotmail.com>
2024-03-13 19:08:55 -0600

    Configuración de la cámara: videojuego en 2D

---
+++ Assets/pixel_adventure/scripts/frog.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frog : MonoBehaviour
+{
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+
+        if (Input.GetKey("space") && checkground.isGround)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+    }
+}
