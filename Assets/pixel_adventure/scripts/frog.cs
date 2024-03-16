7cd6b77d0f256d5d2c9f560e0494e3794abadc77
admin <paccote@hotmail.com>
2024-03-16 09:37:18 -0600

    3.5. El movimiento del jugador

--- Assets/pixel_adventure/scripts/frog.cs
+++ Assets/pixel_adventure/scripts/frog.cs
@@ -8,6 +8,11 @@ public class frog : MonoBehaviour
     public float jumpSpeed = 3;
     Rigidbody2D rb2D;
 
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
+
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -19,19 +24,27 @@ public class frog : MonoBehaviour
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
+
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
         }
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
+
         }
 
         if (Input.GetKey("space") && checkground.isGround)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            animator.SetBool("Run", false);
         }
     }
 }
